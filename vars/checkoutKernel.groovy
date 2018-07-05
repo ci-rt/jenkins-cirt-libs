757ca241d11edfc2e827363e7ec5459dd16040c1
Benedikt Spranger <b.spranger@linutronix.de>
2018-07-05 12:42:29 +0200

    Add kernel checkout helper
    compiletestRunner uses git checkout before each kernel build. This
    takes a lot of time. It is also error prone, since the underlying git
    repository might be changed by a forced push and the commit under test
    may be vanished.
    
    Add a kernel checkout helper as preperatory patch to rework the checkout.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Anna-Maria Gleixner <anna-maria@linutronix.de>

---
+++ vars/checkoutKernel.groovy
@@ -0,0 +1,50 @@
+#!/usr/bin/env groovy
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018 Linutronix GmbH
+/*
+ * CI-RT checkout kernel
+ */
+
+import de.linutronix.cirt.VarNotSetException;
+import de.linutronix.cirt.helper;
+import de.linutronix.cirt.inputcheck;
+
+def call(Map global) {
+	try {
+		node ('kernel') {
+			dir('srcrepo') {
+				deleteDir();
+				unstash(global.STASH_PRODENV);
+				String[] properties = ["environment.properties"];
+				def h = new helper();
+				def gitrepo = "";
+				def gitcheckout = "";
+
+				h.add2environment(properties);
+
+				gitrepo = h.getVar('GITREPO');
+				gitcheckout = h.getVar('GIT_CHECKOUT');
+
+				h = null;
+				dir('src') {
+					gitCheckout(gitrepo, gitcheckout, null);
+				}
+			}
+		}
+	} catch(Exception ex) {
+		if (ex instanceof VarNotSetException) {
+			throw ex;
+		}
+
+		println("kernel checkout failed:");
+		println(ex.toString());
+		println(ex.getMessage());
+		println(ex.getStackTrace());
+		error("kernel checkout failed.");
+	}
+}
+
+def call(String... params) {
+	println params;
+	error("Unknown signature. Abort.");
+}
