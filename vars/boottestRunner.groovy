ci-rt/jenkins-cirt-libs

commit b54ff2dc3b561581531a15e61dacd824b7bd9bf5

boottestRunner: Do not fail test run on consolelog errors
During the reboot of a DUT the serial port communication is logged.
A failure of the logging application during the reboot fail the whole
test run. Be more relaxed, log the failure and continue the test run.

Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
Reviewed-by: Bastian Germann <bage@linutronix.de>

vars/boottestRunner.groovy
@@ -58,7 +58,6 @@ echo $! >'''+""" ${pidfile}"""
 	virshoutput = null;
 	if (output) {
 		println("Virsh console logging Problem (target properly written and logfile writeable?): " + output);
-		/* TODO: throw exception; IT Problem */
 	}
 }
 
@@ -137,9 +136,7 @@ private rebootTarget(String hypervisor, String target, String seriallog,
 	 */
 	if (testboot) {
 		println("kill seriallog");
-		def pid = readFile(pidfile).trim();
-		pidfile = null;
-		sh "kill ${pid}";
+		sh "while ps -p $(cat ${pidfile}); do sudo kill $(cat ${pidfile}); sleep 5; done"
 	}
 }
 

commit f00636c47e437fa7c5329c17260c190c86ada7bc

boottestRunner: Fix command qouting
commit b54ff2dc3b56 ("boottestRunner: Do not fail test run on consolelog
errors") introduced a quoting error in a shell command:

CI-RT/vars/boottestRunner.groovy: 139: illegal string body character after dollar sign;

   solution: either escape a literal dollar sign "\$5" or bracket the value expression "${5}" @ line 139, column 3.

   		sh "while ps -p $(cat ${pidfile}); do sudo kill $(cat ${pidfile}); sleep 5; done"

     ^

1 error

Add the missing escape characters.

Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
Reviewed-by: Bastian Germann <bage@linutronix.de>

vars/boottestRunner.groovy
@@ -136,7 +136,8 @@ private rebootTarget(String hypervisor, String target, String seriallog,
 	 */
 	if (testboot) {
 		println("kill seriallog");
-		sh "while ps -p $(cat ${pidfile}); do sudo kill $(cat ${pidfile}); sleep 5; done"
+		sh "while ps -p \$(cat ${pidfile}); do sudo kill \$(cat ${pidfile}); sl\
+eep 5; done"
 	}
 }
 